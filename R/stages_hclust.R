b6a6ca1b6066e7c028900e95290c3cc60d612bc8
gherardo varando <gherardo.varando@gmail.com>
2025-12-09 16:07:26 +0100

    update hclust with new argument and logic, checking for fastcluster installed

--- R/stages_hclust.R
+++ R/stages_hclust.R
@@ -17,8 +17,13 @@
 #'        the number of stage is performed with respect to the maximization of
 #'        the \code{score} function. \code{NA} and integer can be mixed
 #'        to fix the number of stage for some variables and use the
-#'        score to select others.
+#'        score to select others. If \code{k=NA} the maximum number of stages
+#'        to consider can be set with the \code{max_k} argument.
 #' @param max_k integer, maximum number of stages to consider per variable.
+#' @param first_max logical, if \code{TRUE} and \code{k=NA} for at least
+#'                  some variables, the first local maximum with respect to
+#'                  the number of stages is returned (default to \code{FALSE}
+#'                  equivalent to a global search).
 #' @param method the agglomeration method to be used in \code{\link{hclust}}.
 #' @param limit the maximum number of variables to consider.
 #' @param scope names of the variables to consider.
@@ -31,11 +36,20 @@
 #'          A different number of stages for the different variables
 #'          in the model can be specified by supplying a (named) vector
 #'          via the argument \code{k}.
+#'
 #'          If \code{k} is \code{NA} for some variables, all
-#'          possible number of stages will be checked and the
-#'          one that maximize the \code{score} will be selected.
+#'          possible number of stages (until \code{max_k}) will be checked
+#'          and the one that maximize the \code{score} will be selected.
+#'          If \code{first_max = TRUE} the search for the number of stages
+#'          will stop at the first local maximum encountered.
+#'
 #'          A custom distance can be passed as a function in argument \code{distance}.
-#'          This must resturn an object of class \code{"dist"} similarly to \code{\link{dist}}.
+#'          This must return an object of class \code{"dist"} similarly to
+#'          \code{\link{dist}}.
+#'
+#'          The function \code{\link{hclust}} was used originally, now
+#'          if \pkg{\link{fastcluster}} is available the function
+#'          \code{\link{fastcluster::hclust}} is used.
 #' @return A staged event tree object.
 #' @importFrom stats dist hclust cutree
 #' @examples
@@ -51,6 +65,7 @@ stages_hclust <-
            distance = "totvar",
            k = NA,
            max_k = Inf,
+           first_max = FALSE,
            method = "complete",
            ignore = object$name_unobserved,
            limit = length(object$tree),
@@ -75,9 +90,10 @@ stages_hclust <-
         "x" = "You've supplied {.type {k}}."
       ))
     }
-    if (!requireNamespace("fastcluster", quietly = TRUE)) {
-      hclu <- hclust
+    if (!rlang::is_installed("fastcluster")) {
+      hclu <- stats::hclust
     }else{
+      ## if fastcluster is available, use it
       hclu <- fastcluster::hclust
     }
     if (is.null(scope)) scope <- sevt_varnames(object)[2:limit]
@@ -86,7 +102,9 @@ stages_hclust <-
       k <- rep(k, length(scope))[seq_along(scope)]
       names(k) <- scope
     }
+    scores <- list()
     for (v in scope) {
+      scores[[v]] <- c()
       wch <- names(object$prob[[v]])
       wch <- wch[!(wch %in% ignore)]
       if (length(wch) < 2) next
@@ -101,30 +119,43 @@ stages_hclust <-
                     dist(pp, method = distance)
         )
       }
+      ## do clustering
+      hcres <- hclu(M, method = method)
       if (!is.na(k[v])) {
-        groups <- cutree(hclu(M, method = method), k = min(k[v], attr(M, "Size"), max_k))
+        ## we use the user-supplied k
+        groups <- cutree(hcres, k = min(k[v],attr(M, "Size"), max_k))
+        ### assign stages
+        old <- object$stages[[v]]
+        for (s in 1:k[v]) {
+          object$stages[[v]][old %in% names(which(groups == s))] <- paste0(s)
+        }
+        object <- sevt_fit(object, scope = v)
       } else {
-        hcres <- hclu(M, method = method)
-        res <- lapply(1:min(attr(M, "Size"), max_k), function(k) {
-          groups <- cutree(hcres, k = k)
-          old <- object$stages[[v]]
-          object2 <- object
-          for (s in 1:k) {
-            object2$stages[[v]][old %in% names(which(groups == s))] <- paste0(s)
+        ## search the k that maximize the score
+        old <- object$stages[[v]]
+        old_score <- -Inf
+        for (kk in 1:min(attr(M, "Size"), max_k)){
+          groups <- cutree(hcres, k = kk)
+          new_object <- object
+          for (s in 1:kk) {
+            new_object$stages[[v]][old %in% names(which(groups == s))] <- paste0(s)
           }
-          object2 <- sevt_fit(object2, scope = v)
-          list(score = score(object2), groups = groups)
-        })
-        k[v] <- which.max(sapply(res, function(r) r$score))
-        groups <- res[[k[v]]]$groups
+          new_object <- sevt_fit(new_object, scope = v)
+          new_score <- score(new_object)
+          scores[[v]][kk] <- new_score
+          if (new_score > old_score){
+            ## if improving move to the new_object
+            old_score <- new_score
+            object <- new_object
+          } else if (first_max){
+            ## if not improving and first_max is TRUE exit the loop
+            break
+          }
+          }
       }
-      ### assign stages
-      old <- object$stages[[v]]
-      for (s in 1:k[v]) {
-        object$stages[[v]][old %in% names(which(groups == s))] <- paste0(s)
-      }
-      object <- sevt_fit(object, scope = v)
+
     }
     object$call <- match.call()
+    object$scores <- scores
     return(object)
   }
